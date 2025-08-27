c7c5708f0037b0cbf2cdaec7fee0a8a57b5aa4f6
johncmb <johncmb@gmail.com>
2025-08-27 19:45:16 -0400

    Delete pages directory

--- pages/_app.tsx
+++
@@ -1,5 +0,0 @@
-import type { AppProps } from "next/app";
-import "../app/globals.css"; // reuse the same CSS
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
